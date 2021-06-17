edcab182f28bfd971e67ee4771e637191bc3a27f
Eamonn O'Toole <eamonn.otoole@hpe.com>
2021-06-17 10:28:47 +0100

    Fix hpegl-provider-lib import paths
    In this PR we fix-up the hpegl-provider-lib import paths to use
    hewlettpackard as opposed to hpe-hcss.  We've also updated the go.mod
    module path.

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"github.com/hpe-hcss/hpegl-provider-lib/pkg/registration"
+	"github.com/hewlettpackard/hpegl-provider-lib/pkg/registration"
 )
 
 // ConfigureFunc is a type definition of a function that returns a ConfigureContextFunc object
